caefd8f7e5e7311d73c407ef2976d2ac72f4b662
odpaleniek1337 <deadlodz@wp.pl>
2021-01-26 00:24:45 +0100

    wrote addRent, prototype of deleteRent

--- src/RentingManger.cpp
+++ src/RentingManger.cpp
@@ -1,11 +1,17 @@
 #include "RentingManager.h"
 
 void RentingManager::addRent(Rent newRent) {
-
+    std::shared_ptr<Rent> rent = std::make_shared<Rent>(newRent);
+    RentingManager::rentedItems.push_back(rent);
 }
 
-void RentingManager::deleteRent(Rent oldRent) {
-
+void RentingManager::deleteRent(Rent oldRent/*time_t szef*/) {
+    Rent rent = *RentingManager::rentedItems.back();
+    //get current time- calculate days something was rented
+    //rent.renter
+    //print penalty, info abour who rented etc
+    //no czy ten wektor ma jakies itemy wtedy moze popnac
+    RentingManager::rentedItems.pop_back();
 }
 
 RentingManager::RentingManager() {
