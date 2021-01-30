c3f0afa599bc35533f3867865c9fcb3be2cd1f5a
odpaleniek1337 <deadlodz@wp.pl>
2021-01-30 23:29:27 +0100

    merge with zyksel->reading from file

--- src/main.cpp
+++ src/main.cpp
@@ -127,36 +127,19 @@ void read_Rents(RentingManager &pointer) {
 
 int main() {
     RentingManager es;
-    /*vector<Item> item;
-    Book ksiega = Book(13,"Kordian", "Adam Malysz", 5);
-    item.push_back(ksiega);
-    for(int i=0;i<item.size();i++){
-        //create(item[i]);
-    }
-    //es.addRent(item);
-    Staff maciek = Staff(1,"Maciek");
-    Rent rent1;
-    std::shared_ptr<Staff>maciekptr = std::make_shared<Staff>(maciek);
-    rent1.setRent(ksiega,maciekptr);*/
-
-
     RentingManager *ptr = &es;
     read_Rents(es);
+    /*time_t borrowedTime = time(NULL);
+    struct tm * time = localtime(&borrowedTime);
+    std::cout<<asctime(time)<<std::endl;*/
+    Book ksiega = Book(13,"Kordian", "Adam Malysz", 5);
+    Staff maciek = Staff(1, "Maciek");
+    Rent rent1;
+    std::shared_ptr<Book> ksiazkawojtka = std::make_shared<Book>(ksiega);
+    std::shared_ptr<Staff>maciekptr = std::make_shared<Staff>(maciek);
+    rent1.setRent(ksiazkawojtka,maciekptr);
+    es.addRent(rent1);
     es.deleteRent(1);
     es.deleteRent(2);
-
-    /*std::cout<<wojtek.getAuthor()<<std::endl;
-    std::cout<<wojtek.getID()<<std::endl;
-    //std::cout<<wojtek.getBorrowedTime()<<std::endl;
-    std::cout<<wojtek.getQuantity()<<std::endl;
-    //std::cout<<wojtek.getTitle()<<std::endl;
-    Staff maciek = Staff(1, "Maciek");
-    std::cout<<maciek.getCurrentItems()<<std::endl;
-    std::cout<<maciek.toString()<<std::endl;
-    std::cout<<maciek.getName()<<std::endl;
-    std::cout<<maciek.getID()<<std::endl;
-    */
-    std::cout << "Hello, World!" << std::endl;
     return 0;
-
 }
