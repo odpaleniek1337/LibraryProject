46a0fd1deb0a286c76a71638d5db09fd3d7779d4
odpaleniek1337 <deadlodz@wp.pl>
2021-02-01 01:33:44 +0100

    added addRent, deleteRent buttons, Added label, which shows current Rents, added getRent to rentingManager

--- src/GUIHandler.cpp
+++ src/GUIHandler.cpp
@@ -1,11 +1,17 @@
 #include "GUIHandler.h"
 
-GUIHandler::GUIHandler(std::shared_ptr<DataBase> base, QWidget *parent) : QMainWindow(parent){
+GUIHandler::GUIHandler(std::shared_ptr<DataBase> base, std::shared_ptr<RentingManager> manager, QWidget *parent) : QMainWindow(parent){
     GUIHandler::base = base;
+    GUIHandler::manager = manager;
     updateRentersSize(base->getRentersSize());
     updateItemsSize(base->getItemsSize());
+    updateRentsSize(manager->getSize());
     updateRenters();
     updateItems();
+    updateRents();
+    createChooseButtonUsers();
+    createChooseButtonItems();
+    createRentButtons();
     updateLayout();
 }
 
@@ -57,21 +63,67 @@ void GUIHandler::updateItems() {
 }
 
 
+void GUIHandler::updateRents() {//nie widac, ze dziala bo nie ma zadnych rentow jeszcze
+    rentsView = new QTableView();
+    rentsModel.setHorizontalHeaderLabels({QApplication::translate("rents", "Renter"),
+                                          QApplication::translate("rents", "Title")});
+
+    QList<QStandardItem*> newRentsData;
+    for (int i = 0; i < rentsSize; i++) {
+        std::shared_ptr<Rent> currentRentptr = manager->getRent(i);
+        newRentsData.clear();
+        newRentsData.append(new QStandardItem(QString::fromStdString(currentRentptr->getRenter()->getName())));
+        newRentsData.append(new QStandardItem(QString::fromStdString(currentRentptr->getItem()->getTitle())));
+        rentsModel.appendRow(newRentsData);
+    }
+
+    rentsView->setModel(&rentsModel);
+    rentsView->verticalHeader()->hide();
+
+}
+
 void GUIHandler::updateLayout() {
+
     upperLayout = new QHBoxLayout();
     upperLayout->addWidget(usersView); //creating upperpart of layout
     upperLayout->addWidget(itemsView);
 
+    middleLayout = new QHBoxLayout();
+    middleLayout->addWidget(rentsView);
+    middleLayout->addWidget(deleteRentButton);
+
     lowerLayout = new QHBoxLayout(); //creating lowerpart of layout
+    lowerLayout->addWidget(chooseBoxUser);
+    lowerLayout->addWidget(chooseBoxItem);
+    lowerLayout->addWidget(addRentButton);
 
     mainLayout = new QVBoxLayout();
 
     mainLayout->addLayout(upperLayout);
+    mainLayout->addLayout(middleLayout);
     mainLayout->addLayout(lowerLayout);
 
     auto central = new QWidget;
     central->setLayout(mainLayout);
     setCentralWidget(central);
+}
+
+void GUIHandler::createChooseButtonUsers() {
+    chooseBoxUser = new QComboBox;
+    for(int k=0;k<rentersSize;k++)
+    chooseBoxUser->addItem(QString::fromStdString(base->getRenter(k)->getName()));
+}
+
+void GUIHandler::createChooseButtonItems() {
+    chooseBoxItem = new QComboBox;
+    for(int k=0;k<itemsSize;k++)
+        chooseBoxItem->addItem(QString::fromStdString(base->getItem(k)->getTitle()));
+}
+
+void GUIHandler::createRentButtons() {
+    addRentButton = new QPushButton("Add Rent");
+    deleteRentButton = new QPushButton("Delete Rent");
+
 }
 
 void GUIHandler::updateRentersSize(int size) {
@@ -80,6 +132,10 @@ void GUIHandler::updateRentersSize(int size) {
 
 void GUIHandler::updateItemsSize(int size) {
     itemsSize = size;
+}
+
+void GUIHandler::updateRentsSize(int size) {
+    rentsSize = size;
 }
 
 GUIHandler::~GUIHandler() {
